da45cce16021ecc4dd4bac2999f202739dc4bc8c
Linuzs <freelinuz@gmail.com>
2020-09-02 10:09:38 +0800

    Create 2020-09-02.py

---
+++ 2020-09-02.py
@@ -0,0 +1,13 @@
+number1 = 1
+number2 = 2
+operator = input("Please input operator(+ - * /):")
+if operator == "+":
+    print(number1 + number2)
+elif operator == "-":
+    print(number1 - number2)
+elif operator == "*":
+    print(number1 * number2)
+elif operator == "/":
+    print(number1 / number2)
+else:
+    print("Re input!")
