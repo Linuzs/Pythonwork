324a0acf4ac1ab5c2caffea92cd2a57351dcebc8
Linuzs <freelinuz@gmail.com>
2020-09-02 11:02:35 +0800

    Update 2020-09-02.py

--- 2020-09-02.py
+++ 2020-09-02.py
@@ -11,3 +11,11 @@ elif operator == "/":
     print(number1 / number2)
 else:
     print("Re input!")
+
+#已知10以内3、5的倍数为3，5，6，9，和为23，计算100以内3，5倍数之和
+
+s = 0
+for i in range(1,101):
+    if i % 3 == 0 or i % 5 == 0 :
+        s+=i
+        print(s)
