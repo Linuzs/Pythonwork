82197faf4d45cfa4fe8c367baf6e08efb3f51ede
Linuzs <freelinuz@gmail.com>
2020-09-02 14:56:54 +0800

    Update 2020-09-02.py

--- 2020-09-02.py
+++ 2020-09-02.py
@@ -20,24 +20,32 @@ for i in range(1,101):
         s+=i
         print(s)
 
-#已知10以内3、5的倍数为3，5，6，9，和为23，计算100以内3，5倍数之和
+#随机数猜大小
 
 import random
 
+game = "y"
 count = 0
-
 rand_number = random.randint(1,100)
-while True:
+
+while game == "y":
+    
     number = input("Please your number:")
     while number.isdigit() == True:
+        count+=1
         if int(number) > rand_number:
-            print("The number is too big!%d"%rand_number)
+            print("The number is too big!")
             break
         elif int(number) < rand_number:
-            print("The number is too small!%d"%rand_number)
+            print("The number is too small!")
             break
         elif int(number) == rand_number:
-            print("Good luck!%d"%rand_number)
-
+            print("Good luck! Your count is %d."%count)
+            game = input('Do you want to continue the game yes or no(input y or n):')
+            rand_number = random.randint(1,100)
+            count = 0
+            break
     else:
-        print("Please number!!!")
+        print("Please a number!!!")
+else:
+    print("Game over.")
