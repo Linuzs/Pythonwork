d4d72a2105b92bd77a79d8a333f39c986a744b1f
Linuzs <freelinuz@gmail.com>
2020-09-02 10:08:55 +0800

    Update 2020-09-01.py

--- 2020-09-01.py
+++ 2020-09-01.py
@@ -19,3 +19,19 @@ for i in range(4):
     num = random.randint(0,len(strlist))
     code += strlist[num]
 print(code)
+
+username = input("Please input your name:")
+password = input("Please input your password:")
+hide_password = len(password)
+
+if username == "":
+    username = "Player1"
+if password == "":
+    print("Please input your password!!!")
+    exit()
+
+print("########################################")
+print("#Welcome to the invincible Heroes game!#")
+print("#Your username is %s!"%(username))
+print("#Your password is %s!"%(hide_password * "*"))
+print("########################################")
