469d11db30d220a08efdf4876d936db99ef1642c
Linuzs <freelinuz@gmail.com>
2020-09-03 09:23:53 +0800

    Create 2020-09-03.py

---
+++ 2020-09-03.py
@@ -0,0 +1,13 @@
+i = ""
+list = []
+while i != "q":
+    i = input("input:")
+    list.append(i)
+    print(list)
+else:
+    print("quit.")
+    
+    
+倒序
+list = [1, 2, 3, 4, 5]
+print(list[::-1])
