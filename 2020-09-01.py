6767953736c8a2e3a96d68e1a68efd0feb5b8ae0
Linuzs <freelinuz@gmail.com>
2020-09-07 17:00:52 +0800

    Update 2020-09-01.py

--- 2020-09-01.py
+++ 2020-09-01.py
@@ -37,3 +37,76 @@ print("#Your password is %s!"%(hide_password * "*"))
 print("########################################")
 
 https://www.cloudflare.com/
+    
+    
+    
+    
+    
+    
+    username = input("Please input your name:")
+password = input("Please input your password:")
+repeat_password = input("Please input your repeat password:")
+
+error_password = 0
+while error_password < 3:
+    if username.strip() == "":
+        username = "Player1"
+    elif password.strip() != "" and repeat_password.strip() != "" and repeat_password.strip() == password.strip():
+        print("register success")
+        f = open("info.txt", 'a+')
+        f.write(username + ' ' + password + '\n')
+        f.close()
+        print("Please login")
+        username = input("username:")
+        password = input("password:")
+        f2 = open("info.txt",'r')
+        res = f2.read()
+        #res.close()
+        user_list = res.split()
+        user_dict = {}
+        for i in user_list:
+            username = i.split(',')[0]
+            password = i.split(',')[1]
+            user_dict[username] = password
+            print(user_dict)
+        break
+    else:
+        print("error pass")
+        break
+'''
+
+
+info = {}
+        info['username'] = username
+        info['password'] = password
+        print(info)
+        break
+    
+    
+    
+info = {}
+info['username'] = username
+info['password'] = password
+print(info)
+ 
+ '''   
+'''
+print("########################################")
+print("#Welcome to the invincible Heroes game!#")
+print("#Your username is %s!"%(username))
+print("#Your password is %s!"%(len(password) * "*"))
+print("########################################")
+
+info = {}
+info['username'] = username
+info['password'] = password
+print(info)
+
+
+dict = {'username':'admin','password':'123456'}
+username = 'admin'
+if username in dict:
+    print("x")
+else:
+    print("dui")
+    '''
