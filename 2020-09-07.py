d143d9c5caa2aada2e9be955e76f77e09934c285
Linuzs <freelinuz@gmail.com>
2020-09-07 09:54:05 +0800

    Create 2020-09-07.py

---
+++ 2020-09-07.py
@@ -0,0 +1,6 @@
+ll = [i for i in range(10) if i % 3 == 0 or i % 5 == 0]
+print(ll)
+
+print(sum([i for i in range(10) if i % 3 == 0 or i % 5 == 0]))
+
+print(sum([i for i in range(1,1000) if i % 3 == 0 or i % 5 == 0]))
