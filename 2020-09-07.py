b9cb8b1850355b3b2a7e1692a2e49fddabc16767
Linuzs <freelinuz@gmail.com>
2020-09-07 15:28:59 +0800

    Update 2020-09-07.py

--- 2020-09-07.py
+++ 2020-09-07.py
@@ -4,3 +4,37 @@ print(ll)
 print(sum([i for i in range(10) if i % 3 == 0 or i % 5 == 0]))
 
 print(sum([i for i in range(1,1000) if i % 3 == 0 or i % 5 == 0]))
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 10:31:29 2020
+
+@author: tute
+"""
+
+world = [['#','#','#','#','#','#'],['#','#','#','#','#','#'],['#','#','#','#','#','#']]
+
+up_down = 0
+left_right = 0
+
+while True:        
+    world[up_down][left_right] = "*"
+    print("Your are here:")
+    print("".join(world[0]))
+    print("".join(world[1]))
+    print("".join(world[2]))
+    user_input = input("wads:")
+    
+    if user_input == "w":
+        world[up_down][left_right] = "#"
+        up_down -= 1
+    elif user_input == "a":
+        world[up_down][left_right] = "#"
+        left_right -= 1
+    elif user_input == "s":
+        world[up_down][left_right] = "#"
+        up_down += 1
+    elif user_input == "d":
+        world[up_down][left_right] = "#"
+        left_right += 1    
